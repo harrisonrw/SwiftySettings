c3131bcd47976a3d3c07309fe518a04a53c0a337
Robert Harrison <harrisonrw@gmail.com>
2020-12-11 11:07:43 -0800

    Initial Project Files

---
+++ SwiftySettings/SwiftySettings/SwiftySettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftySettingsApp.swift
+//  SwiftySettings
+//
+//  Created by Robert Harrison on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftySettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
